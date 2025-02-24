dd6e913bfdc351de5223a1b24086e17ce6aef47e
Sebastian Ingino <ingino@stanford.edu>
2025-02-24 15:55:45 -0800

    Fix command parsing

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -2,4 +2,6 @@ from discord.ext import commands
 from .project import Project
 
 def register(bot: commands.Bot):
-    bot.add_command(commands.command(name=Project.name.lower(), help=Project.help)(Project.entry))
+    @bot.command(name=Project.name.lower(), help=Project.help)
+    async def project_entry(ctx: commands.Context, *args: str):
+        await Project.entry(ctx, *args)
