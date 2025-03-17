fc85d470e257aa5699f36cbb8facd770d65a1ef7
Sebastian Ingino <ingino@stanford.edu>
2025-03-16 20:32:04 -0700

    Fix datetime again

--- src/mistral/agent.py
+++ src/mistral/agent.py
@@ -153,7 +153,7 @@ class AgentModel:
         )
         prompt.append(
             SystemMessage(
-                content=f"The current datetime is {datetime.now().isoformat()} in UTC. The user is in PDT."
+                content=f"The current datetime is {datetime.now().isoformat()} in UTC. The user is in PDT (UTC-7)."
             )
         )
 
